a28a7c2c830582030e428bbd45011e9d4cd04201
NehaSharma-10 <shrmatwinkle246@gmail.com>
2022-07-31 08:35:12 +0530

    Trigger rebuild

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 window.onload = function () {
   const yes = document.getElementById("main-img");
   const image1 = () => {
-    yes.src = "./assets/image1.jpg";
+    yes.src = "assets/image1.jpg";
   };
   document.getElementById("img-1").addEventListener("click", image1);
   document.getElementById("img-1").addEventListener("mouseenter", image1);
 
   const image2 = () => {
-    yes.src = "./assets/image2.jpg";
+    yes.src = "assets/image2.jpg";
   };
   document.getElementById("img-2").addEventListener("click", image2);
   document.getElementById("img-2").addEventListener("mouseenter", image2);
 
   const image3 = () => {
-    yes.src = "./assets/image3.jpg";
+    yes.src = "assets/image3.jpg";
   };
   document.getElementById("img-3").addEventListener("click", image3);
   document.getElementById("img-3").addEventListener("mouseenter", image3);
 
   const image4 = () => {
-    yes.src = "./assets/image4.jpg";
+    yes.src = "assets/image4.jpg";
   };
   document.getElementById("img-4").addEventListener("mouseenter", image4);
   document.getElementById("img-4").addEventListener("click", image4);
 
   const image5 = () => {
-    yes.src = "./assets/image5.jpg";
+    yes.src = "assets/image5.jpg";
   };
   document.getElementById("img-5").addEventListener("click", image5);
   document.getElementById("img-5").addEventListener("mouseenter", image5);
 
   const image6 = () => {
-    yes.src = "./assets/image6.jpg";
+    yes.src = "assets/image6.jpg";
   };
   document.getElementById("img-6").addEventListener("click", image6);
   document.getElementById("img-6").addEventListener("mouseenter", image6);
